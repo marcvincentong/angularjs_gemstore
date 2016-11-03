e13bc48bed7c98a78e01fbf2a19f961e66e6ca5d
Marc <marcvincentong@gmail.com>
2016-11-03 23:39:30 +0800

    Add built-in services

--- products.js
+++ products.js
@@ -1,57 +1,57 @@
-(function() {
-  var app = angular.module("store-directives", []);
-  app.directive("productDescription", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-description.html"
-    };
-  });
+(function(){
+    var app = angular.module('store-directives', []);
 
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
+    app.directive("productDescription", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-description.html"
+      };
+    });
 
-  app.directive("productSpecs", function() {
-    return {
-      restrict:"A",
-      templateUrl: "product-specs.html"
-    };
-  });
+    app.directive("productReviews", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-reviews.html"
+      };
+    });
 
-  app.directive("productTabs", function() {
-    return {
-      restrict: "E",
-      templateUrl: "product-tabs.html",
-      controller: function() {
-        this.tab = 1;
+    app.directive("productSpecs", function() {
+      return {
+        restrict:"A",
+        templateUrl: "product-specs.html"
+      };
+    });
 
-        this.isSet = function(checkTab) {
-          return this.tab === checkTab;
-        };
+    app.directive("productTabs", function() {
+      return {
+        restrict: "E",
+        templateUrl: "product-tabs.html",
+        controller: function() {
+          this.tab = 1;
 
-        this.setTab = function(activeTab) {
-          this.tab = activeTab;
-        };
-      },
-      controllerAs: "tab"
-    };
-  });
+          this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+          };
 
-  app.directive("productGallery", function() {
-    return {
-      restrict: "E",
-      templateUrl: "product-gallery.html",
-      controller: function() {
-        this.current = 0;
-        this.setCurrent = function(imageNumber){
-          this.current = imageNumber || 0;
-        };
-      },
-      controllerAs: "gallery"
-    };
-  });
+          this.setTab = function(activeTab) {
+            this.tab = activeTab;
+          };
+        },
+        controllerAs: "tab"
+      };
+    });
 
-})();
+    app.directive("productGallery", function() {
+      return {
+        restrict: "E",
+        templateUrl: "product-gallery.html",
+        controller: function() {
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+        },
+        controllerAs: "gallery"
+      };
+    });
+  })();
